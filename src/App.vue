c435ebbf8b9a15c9bac2ede35990c343f4b33da6
Thibaud Maciuk <thibaud.maciuk@le-campus-numerique.fr>
2023-07-27 18:56:52 +0200

    Init portfolio

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/general/menu.vue'
+</script>
+
+<template>
+  <Menu />
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/general/menu.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <!-- Div conteuner du composant-->
+  <div class="w-screen flex justify-center bg-amber-200">
+
+    <!-- Div du menu qui est centré-->
+    <div>
+      <router-link to="/">Home</router-link>
+      <a>Linkedin</a>
+      <a>Github</a>
+    </div>
+
+  </div>
+<br />
+  <h1>Menu</h1>
+</template>
